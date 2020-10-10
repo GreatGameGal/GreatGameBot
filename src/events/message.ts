54bb7c6e02642c31f20bf0af4f5ef3cb492256ad
GreatGameGal <thegreatgamegal@gmail.com>
2020-10-10 06:53:55 -0700

    Changed how permissions were done and created default user data.

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,8 +1,14 @@
 import Discord from "discord.js";
+import defaultUserData from "../defaultUserData";
 
 export async function run(this: Bot, message: Discord.Message) {
-  let userPermissionData =  await this.mongo.db("user").collection("permissions").findOne({id: message.author.id});
-  if (userPermissionData && userPermissionData.banned) return;
+  let userData =  await this.mongo.db("bot").collection("user").findOne({id: message.author.id});
+  if (userData == null) {
+    userData = {id: message.author.id};
+    Object.assign(userData, defaultUserData);
+    this.mongo.db("bot").collection("user").insertOne(userData);
+  }
+  if (userData.globalPermissions < 0) return;
   if (!message.author.bot) {
     for (let i = 0; i < this.config.prefixes.length; i++) {
       const prefix = this.config.prefixes[i];
